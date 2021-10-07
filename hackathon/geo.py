d673376f0f76106f795421604607bfe878f8283c
hasnobrains <lisichkin.ilya@gmail.com>
2021-10-07 19:36:02 +0300

    Added new json data and all code in geo.py

---
+++ hackathon/geo.py
@@ -0,0 +1,66 @@
+import json
+import sys
+import pandas as pd
+import numpy as np
+from geopy.geocoders import Nominatim
+from flask import Flask
+import folium
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+
+    geolocator = Nominatim(user_agent='russia_explorer')
+    filename = sys.argv[1]
+    try:
+        with open(filename) as f:
+            data = json.load(f)
+    except IOError:
+        print(f"File {filename} not found!\n")
+        return
+
+    #  print(data)
+
+    df = pd.json_normalize(data['events'])
+    #  print(df.info())
+
+    # shrink whitespaces
+    df.replace(np.nan, '', inplace=True)
+    for index, row in df.iterrows():
+        row['address.region'] = ' '.join((row['address.region']
+                                          .split()))
+        row['address.place'] = ' '.join((row['address.place']
+                                         .split()))
+        row['address.street'] = ' '.join((row['address.street']
+                                          .split()))
+        row['address.building'] = ' '.join((row['address.building']
+                                            .split()))
+
+        if row['coordinates.x'] == '' and row['coordinates.y'] == '':
+            address = f"улица {row['address.street']} "
+            address += f"{row['address.building']}, "
+            address += f"{row['address.place']}, "
+            address += f"{row['address.region']}"
+            location = geolocator.geocode(address)
+            df.loc[df['id'] == row['id'],
+                   'coordinates.x'] = location.longitude
+            df.loc[df['id'] == row['id'],
+                   'coordinates.y'] = location.latitude
+
+    folium_map = folium.Map(location=[55.0, 103.0], zoom_start=4)
+    for latitude, longitude, title in zip(df['coordinates.y'],
+                                          df['coordinates.x'],
+                                          df['title']):
+        label = folium.Popup(title, parse_html=True)
+        #  print(f"{latitude}, {longitude}")
+        folium.Marker([latitude, longitude],
+                      popup=label).add_to(folium_map)
+
+    return folium_map._repr_html_()
+
+
+if __name__ == '__main__':
+    #  index()
+    app.run(debug=True)
