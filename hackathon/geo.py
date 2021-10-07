c003f3db468969c9cebac72e9248b096191f4190
hasnobrains <lisichkin.ilya@gmail.com>
2021-10-07 21:30:52 +0300

    Save commit

--- hackathon/geo.py
+++ hackathon/geo.py
@@ -5,6 +5,7 @@ import numpy as np
 from geopy.geocoders import Nominatim
 from flask import Flask
 import folium
+from folium.plugins import MarkerCluster
 
 app = Flask(__name__)
 
@@ -21,7 +22,8 @@ def index():
         print(f"File {filename} not found!\n")
         return
 
-    #  print(data)
+    pretty = json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False)
+    print(pretty)
 
     df = pd.json_normalize(data['events'])
     #  print(df.info())
@@ -50,14 +52,30 @@ def index():
                    'coordinates.y'] = location.latitude
 
     folium_map = folium.Map(location=[55.0, 103.0], zoom_start=4)
-    for latitude, longitude, title in zip(df['coordinates.y'],
-                                          df['coordinates.x'],
-                                          df['title']):
+    #  folium.GeoJson(
+    #      '/Volumes/Second/Downloads/russia_geojson/admin_level_4.geojson',
+    #      name="geojson").add_to(folium_map)
+    # adding layers
+    layer = {}
+    for type in df['type'].drop_duplicates():
+        layer[type] = folium.FeatureGroup(name=type)
+        folium_map.add_child(layer[type])
+
+    for latitude, longitude, title, type, special in zip(df['coordinates.y'],
+                                                         df['coordinates.x'],
+                                                         df['title'],
+                                                         df['type'],
+                                                         df['special']):
         label = folium.Popup(title, parse_html=True)
         #  print(f"{latitude}, {longitude}")
+        icon = folium.Icon(color="blue")
+        if special is True:
+            icon = folium.Icon(color="red", icon="info-sign")
+        marker_cluster = MarkerCluster().add_to(layer[type])
         folium.Marker([latitude, longitude],
-                      popup=label).add_to(folium_map)
-
+                      icon=icon,
+                      popup=label).add_to(marker_cluster)
+    folium.LayerControl().add_to(folium_map)
     return folium_map._repr_html_()
 
 
